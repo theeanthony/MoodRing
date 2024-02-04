4fd2e2dec5999996d4b51cc27a34c2e12e88d4a9
Anthony <acontreras.sing@gmail.com>
2024-02-03 19:00:03 -0800

    Initial Commit

---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by Anthony Contreras on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
